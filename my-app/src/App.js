3282040369d3f9c6a806b0cd53c10b7093f5dc99
michaelvega <michaelvega8888@users.noreply.github.com>
2024-01-21 00:45:35 -0500

    Add bottom nav Update App.css Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,7 +11,8 @@ function App() {
             <p className="Text-title">kush.</p>
             <p className="text1" style={{fontWeight: "normal"}}>Weed on speed dial.</p>
             <div className="Text-holder2">
-              <p className="text2" style={{fontWeight: "lighter", marginBottom: "1.5rem"}}> We're over it and you should be too.</p>
+              <p className="text2" style={{fontWeight: "lighter", marginBottom: "1.5rem"}}> We're over it and you should
+                be too.</p>
               <p className="text2"> That's why we created <b>kush.</b></p>
               <p className="text2"><u>Learn More <b>→</b></u></p>
               <p className="text2"><b>Join the waitlist</b></p>
@@ -20,6 +21,14 @@ function App() {
           </div>
           <div className="Text-holder" style={{width: "30%"}}>
             <p>hello</p>
+          </div>
+
+          <div className={"bottomnavWrapper"}>
+            <center>
+              <div className="bottomnav">
+                <p className="text2" style={{fontSize: "24px"}}>What's kush? <b>↓</b></p>
+              </div>
+            </center>
           </div>
         </div>
 
