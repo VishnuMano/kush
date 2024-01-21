3738a5322159dd79782865cad48924fb7fee6b80
michaelvega <michaelvega8888@users.noreply.github.com>
2024-01-21 01:52:05 -0500

    first screen frontend done Update App.css Update App.js Add logo.png

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import logo from './assets/imgs/logo.png';
+
 import './App.css';
 
 function App() {
@@ -6,30 +7,38 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="Main-frame">
-
-          <div className="Text-holder" style={{width: "50%"}}>
-            <p className="Text-title">kush.</p>
-            <p className="text1" style={{fontWeight: "normal"}}>Weed on speed dial.</p>
-            <div className="Text-holder2">
-              <p className="text2" style={{fontWeight: "lighter", marginBottom: "1.5rem"}}> We're over it and you should
-                be too.</p>
-              <p className="text2"> That's why we created <b>kush.</b></p>
-              <p className="text2"><u>Learn More <b>→</b></u></p>
-              <p className="text2"><b>Join the waitlist</b></p>
-
-            </div>
-          </div>
-          <div className="Text-holder" style={{width: "30%"}}>
-            <p>hello</p>
-          </div>
-
-          <div className={"bottomnavWrapper"}>
-            <center>
-              <div className="bottomnav">
-                <p className="text2" style={{fontSize: "24px"}}>What's kush? <b>↓</b></p>
+          <div className="Two-col" style={{width: "40%"}}>
+            <div className="Text-holder">
+              <p className="Text-title">kush.</p>
+              <p className="text1" style={{fontWeight: "normal"}}>Weed on speed dial.</p>
+              <div className="Text-holder2">
+                <p className="text2" style={{fontWeight: "lighter", marginBottom: "1.5rem"}}> We're over it and you
+                  should
+                  be too.</p>
+                <p className="text2"> That's why we created <b>kush.</b></p>
+                <p className="text2"><u>Learn More <b>→</b></u></p>
+                <p className="text2"><b>Join the waitlist</b></p>
               </div>
-            </center>
+            </div>
+
+              <div className="email-signup" >
+                <input type="email" className="email-input" placeholder="Email"/>
+                <button type="submit" className="signup-button">I'm in!</button>
+              </div>
+
           </div>
+
+          <div className="Text-holder" style={{width: "40%"}}>
+            <img src={logo} className="App-logo" alt="logo"/>
+          </div>
+        </div>
+
+        <div className={"bottomnavWrapper"}>
+          <center>
+            <div className="bottomnav">
+              <p className="text2" style={{fontSize: "24px"}}>What's kush? <b>↓</b></p>
+            </div>
+          </center>
         </div>
 
       </header>
